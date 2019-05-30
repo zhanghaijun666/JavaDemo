980b97c22a652514765f938d20255a421df7fae7
zhanghaijun <zhanghaijun@bjtxra.com>
2019-05-30 15:51:51 +0800

    solr

---
+++ src/main/java/com/solr/Person.java
@@ -0,0 +1,42 @@
+package com.solr;
+
+import org.apache.solr.client.solrj.beans.Field;
+
+/**
+ * @author zhanghaijun
+ */
+public class Person {
+
+    @Field(value = "id")
+    private String id;
+
+    @Field(value = "title")
+    private String name;
+
+    @Field(value = "description")
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
